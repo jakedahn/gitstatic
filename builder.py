a5d02144d77dacc0befd956c11cb62747ebfef9d
jakedahn <jake@markupisart.com>
2014-08-01 23:08:48 -0700

    killing gitpython, moving to check_call

--- builder.py
+++ builder.py
@@ -7,9 +7,6 @@ import tempfile
 import yaml
 
 
-from git import *
-
-
 def parse_args():
     ap = argparse.ArgumentParser(description='Build static sites gitstatic style')
     ap.add_argument('--git-url',
@@ -32,13 +29,14 @@ def main():
     tmpdir_build_path = '%s/gitstatic_build' % tmpdir
     gitstatic_path = '%s/.gitstatic.yml' % tmpdir
 
-    repo = Repo.clone_from(args.git_url, tmpdir) # clone git repo
-    # repo.remotes.origin.fetch() # fetch origin
-    repo.git.checkout('origin/%s' % args.git_ref, b=args.git_ref) # reset to desired git ref
+    subprocess.check_call(['git', 'clone', args.git_url, tmpdir])
+    subprocess.check_call(['git', 'fetch', 'origin'])
+    subprocess.check_call(['git', 'checkout', 'origin/%s' % args.git_ref])
+
     if not os.path.isfile(gitstatic_path):
         raise SystemExit('Error: The repo you are trying to build does NOT '
                          'contain a .gitstatic.yml manifest file.')
-    #FIXME(jake): the original builder.sh would grab submodules, we probably want this here too
+    subprocess.check_call(['git', 'submodule', 'update', '--init'])
 
     # parse gitstatic yml
     with open(gitstatic_path, 'r') as f:
@@ -48,18 +46,14 @@ def main():
     deploy_path = '%s/%s' % (args.web_root, cname)
     build_script = build_vars.get('build_command').split(' ')
 
-    res = subprocess.call(build_script)
+    subprocess.check_call(build_script)
 
-    if res != 0:
-        raise SystemExit('Error: Something broke during build phase')
+    rsync_cmd = 'rsync --recursive --update --delete --perms %s/* %s' % \
+                (tmpdir_build_path, deploy_path)
+    rsync_res = subprocess.check_call(rsync_cmd.split(' '))
 
-    rsync_cmd = 'rsync --recursive --update --delete --perms %s %s' % (tmpdir_build_path, deploy_path)
-    rsync_res = subprocess.call(rsync_cmd.split(' '))
-
-    if rsync_res != 0:
-        raise SystemExit('Error: Something broke during rsync')
-
-    print 'Success! %s has been built and is now hosted at %s from path %s' % (args.git_url, cname)
+    print 'Success! %s has been built and is now hosted at %s from path %s' % \
+          (args.git_url, cname)
 
 
 if __name__ == '__main__':
